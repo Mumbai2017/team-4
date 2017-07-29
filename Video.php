43373db70f3957d2106924f1d916187891a2abeb
Devanshi <devanshihd20@gmail.com>
2017-07-29 20:16:51 +0530

    Video

---
+++ Video.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head><title>View Video</title>
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+  <div class="row">
+  <div class="col-lg-3"></div>
+  <div class="col-lg-9">
+  <div class="panel-group">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1">Teacher name:<span id="tname">devanshi</span></a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse">
+      <div class="panel-body">Youtube</div>
+      <div class="panel-footer">
+          <button type="button" class="btn btn-default">Approve</button>
+          <button type="button" class="btn btn-default">Review</button>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+</div>    
+    </body>
+
+
+
+</html>
+
