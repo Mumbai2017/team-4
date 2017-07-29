a0a8cd13d53d0a760d96d6296524e00f6e5dccbb
hanare <prince06.1996@rediffmail.com>
2017-07-29 22:30:43 +0530

    Chat UI

---
+++ PHP-API/php_action/chatt.php
@@ -0,0 +1,28 @@
+<div class= "col-md-3">
+        <div id="pchat">
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            Chat Window
+            </button>
+            <div class="collapse" id="collapseExample">
+            <div class="panel panel-default">
+             <div class="panel-body">
+             <div class="input-group">
+      <input type="text" class="form-control">
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-default" >Send</button>
+        </div>
+        </div>                 </div>
+            </div>
+            
+                <div class="well">
+                    <?php echo "chat message ";?>
+                 </div>
+        </div>
+        </div>
+    <div>
+    </div>
+</div>
+
+
+
+    
