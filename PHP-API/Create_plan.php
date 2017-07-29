346b5008f06b4cbeb4967424a9a0eb4b471c7466
Devanshi <devanshihd20@gmail.com>
2017-07-29 21:05:55 +0530

    Create plan

---
+++ PHP-API/Create_plan.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head><title>View Video</title>
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        #btn1{
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+    </style>
+    </head>
+    <body>
+  <div class="row">
+  <div class="col-lg-3"></div>
+  <div class="col-lg-9">
+      <button id="btn1" type="button" class="btn btn-primary btn-md"> <span class="glyphicon glyphicon-plus"></span>Create a new plan</button>
+  <div class="panel-group">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1">Plan ID:<span id="tname">01</span></a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse">
+      <div class="panel-body">Plan name:</div>
+      <div class="panel-footer">
+      <div class="progress">
+             <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+      </div>
+      </div>
+          
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="panel-group">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1">Plan ID:<span id="tname">01</span></a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse">
+      <div class="panel-body">Plan name:</div>
+      <div class="panel-footer">
+      <div class="progress">
+             <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+      </div>
+      </div>
+          
+      </div>
+    </div>
+  </div>
+</div>
+      
+        </div>
+</div>    
+    </body>
+
+
+
+</html>
+
