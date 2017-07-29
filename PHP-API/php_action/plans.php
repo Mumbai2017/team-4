2b138c23c0a355ad8fcd26381deefe01aabe635a
LAPTOP-2F8A9606\Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-29 21:26:30 +0530

    added changes in plan.php

--- PHP-API/php_action/plans.php
+++ PHP-API/php_action/plans.php
@@ -15,72 +15,83 @@ $plan_in_db = $connect->query($plan);
 ?>
 <html>
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Admin Section</title>
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Section</title>
 
-        <!-- bootstrap -->
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-        <!-- bootstrap theme-->
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-theme.min.css">
-        <!-- font awesome -->
-        <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
-
-
-        <!-- DataTables -->
-        <link rel="stylesheet" href="../assets/plugins/datatables/jquery.dataTables.min.css">
-
-        <!-- file input -->
-        <link rel="stylesheet" href="../assets/plugins/fileinput/css/fileinput.min.css">
-
-        <!-- jquery -->
-        <script src="../assets/jquery/jquery.min.js"></script>
-        <!-- jquery ui -->
-        <link rel="stylesheet" href="../assets/jquery-ui/jquery-ui.min.css">
-
-        <script src="../assets/jquery-ui/jquery-ui.min.js"></script>
-
-        <!-- bootstrap js -->
-        <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <!-- bootstrap theme-->
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-theme.min.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
 
 
-    </head>
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../assets/plugins/datatables/jquery.dataTables.min.css">
+
+    <!-- file input -->
+    <link rel="stylesheet" href="../assets/plugins/fileinput/css/fileinput.min.css">
+
+    <!-- jquery -->
+    <script src="../assets/jquery/jquery.min.js"></script>
+    <!-- jquery ui -->
+    <link rel="stylesheet" href="../assets/jquery-ui/jquery-ui.min.css">
+
+    <script src="../assets/jquery-ui/jquery-ui.min.js"></script>
+
+    <!-- bootstrap js -->
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
 
 <body>
-    <div class="form-group">
-        <label for="subject" class="control-label">SUBJECT</label><br>
-        <?php
-            if($subject_in_db->num_rows > 0){
-                while($row = $subject_in_db->fetch_array()){
-                    ?>
-                    <div class="checkbox-inline">
-                        <label class="checkbox-inline">
-                            <input type="checkbox" value="<?php echo $row['subject'];?>" name="subject" class="subject" id="subject" onclick="changed();"><?php echo $row['subject']; ?>
-                        </label>
-                    </div>
-               <?php
-                }
-            }
-        ?>
-    </div>
-
-    <div class="row">
-        <div class="col-md-10">
+<div class="form-group">
+    <label for="subject" class="control-label">SUBJECT</label><br>
+    <?php
+    if($subject_in_db->num_rows > 0){
+        while($row = $subject_in_db->fetch_array()){
+            ?>
+            <div class="checkbox-inline">
+                <label class="checkbox-inline">
+                    <input type="checkbox" value="<?php echo $row['subject'];?>" name="subject" class="subject" id="subject" onclick="changed();"><?php echo $row['subject']; ?>
+                </label>
+            </div>
             <?php
-                if($plan_in_db->num_rows > 0){
-                    while ($row = $plan_in_db->fetch_array()){
+        }
+    }
+    ?>
+</div>
+
+<div class="row">
+    <div class="col-md-10">
+        <?php
+        if($plan_in_db->num_rows > 0){
+            while ($row = $plan_in_db->fetch_array()){
+                $sql = "SELECT * FROM users WHERE id=".$row['user_id'];
+                $data = $connect->query($sql);
+                if($data->num_rows > 0){
+                    while($my_row = $data->fetch_array()){
                         ?>
                         <ul class="list-group">
                             <div class="plan">
-                                <li class="list-group-item" id="<?php echo $row[''] ?>"></li>
+                                <li class="list-group-item" id="<?php echo $row['id']?>"><blockquote><p><?php echo $my_row['name']; ?></p><button class="btn btn-default" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;"><span>View Document</span></button></blockquote>
+                                    <button class="btn btn-default" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;" > <i class="glyphicon glyphicon-heart" data-aos="flip-right"></i><span>Approve</span></button>
+                                    <button class="btn btn-default comment" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;"><i class="glyphicon glyphicon-flash" style="color:#f9d616;"></i><span style="color:#f9d616;">Chat</span></button>
+                                </li>
                             </div>
                         </ul>
-                    <?php
+                        <?php
                     }
                 }
-            ?>
-        </div>
+                ?>
+                <?php
+            }
+        }
+        ?>
     </div>
+</div>
 <script src="../custom/js/plans.js"></script>
 </body>
 </html>
