bae34f95736ab6713c8c673fcb0b890fc4ba408b
LAPTOP-2F8A9606\Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-29 17:22:18 +0530

    migration files created for database

--- API/database/migrations/2017_07_29_111025_create_subject_table.php
+++ API/database/migrations/2017_07_29_111025_create_subject_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSubjectTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create('subject', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('mobile_number')->nullable();
-            $table->string('type_of_user');
-            $table->timestamps();
+            $table->string('subject');
         });
     }
 
@@ -31,6 +27,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('subject');
     }
 }
