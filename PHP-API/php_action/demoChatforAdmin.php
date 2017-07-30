2853b7d6fd15a6f04a2e8c8dd9cbe622a84cb505
LAPTOP-2F8A9606\Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-30 10:31:44 +0530

    upd

--- PHP-API/php_action/demoChatforAdmin.php
+++ PHP-API/php_action/demoChatforAdmin.php
@@ -6,11 +6,7 @@ $a_id = $_SESSION['user_id'];
 $string = "Teacher".$id.$a_id;
 
 ?>
-<style>
-body {
-	background-image : url("../../unnamed.jpg");
-}
-</style>
+
 
 <div id="tlkio" data-channel="<?php echo $string; ?>" data-theme="theme--minimal" style="width:250px;height:250px;position: fixed;bottom: 25px;right: 0;"></div><script async src="http://tlk.io/embed.js" type="text/javascript"></script></div>
 
