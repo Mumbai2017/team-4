86397d3d8dead361456e2613641eebe9a0ce54be
LAPTOP-2F8A9606\Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-30 06:51:34 +0530

    email on approval

--- PHP-API/php_action/updateStatus.php
+++ PHP-API/php_action/updateStatus.php
@@ -1,10 +1,21 @@
 <?php
 
 include 'db_connect.php';
-
+include '../Mail.php';
+global $email;
 $id = $_POST['id'];
-echo $id;
+
 $sql = "UPDATE plan SET approved_status = 1 WHERE id=".$id ;
 $query = $connect->query($sql);
+
+$sql_2 = "SELECT * FROM plan WHERE user_id=".$id;
+$query = $connect->query($sql_2);
+
+if($query->num_rows > 0){
+    while ($row = $query->fetch_array()){
+        $email = $row['email'];
+    }
+}
+Mail::sendMail('Your Plan has been Successfully verified','Thankyou',$email);
 
 ?>
