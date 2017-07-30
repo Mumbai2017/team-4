a516110d56bc0d9bfe672f43b4f565853537dde5
LAPTOP-2F8A9606\Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-30 07:08:49 +0530

    resolced mail conflicts

--- PHP-API/php_action/updateStatus.php
+++ PHP-API/php_action/updateStatus.php
@@ -2,20 +2,26 @@
 
 include 'db_connect.php';
 include '../Mail.php';
-global $email;
+
 $id = $_POST['id'];
 
 $sql = "UPDATE plan SET approved_status = 1 WHERE id=".$id ;
 $query = $connect->query($sql);
 
-$sql_2 = "SELECT * FROM plan WHERE user_id=".$id;
-$query = $connect->query($sql_2);
+$sql_2 = "SELECT * FROM plan WHERE id=".$id;
+$query_2 = $connect->query($sql_2);
 
-if($query->num_rows > 0){
-    while ($row = $query->fetch_array()){
-        $email = $row['email'];
+if($query_2->num_rows > 0) {
+    while ($row = $query_2->fetch_array()) {
+        $sql_3 = "SELECT * FROM users WHERE id=".$row['user_id'];
+        $query_3 = $connect->query($sql_3);
+        if($query_3->num_rows > 0){
+            while ($row_2 = $query_3->fetch_array()){
+                $email = $row_2['email'];
+                Mail::sendMail('Your Plan has been Successfully verified','Thankyou',$email);
+            }
+        }
     }
 }
-Mail::sendMail('Your Plan has been Successfully verified','Thankyou',$email);
 
 ?>
