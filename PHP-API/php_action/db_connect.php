c9bbeac8c5a2f9bf739d92530313a19a3570f4ab
Dipen Ved <vedipen@gmail.com>
2017-07-30 07:36:16 +0530

    Edit config

--- PHP-API/php_action/db_connect.php
+++ PHP-API/php_action/db_connect.php
@@ -1,15 +1,21 @@
- <?php 
+ <?php
 /**
  * Created by PhpStorm.
  * User: Sumeet
  * Date: 29-07-2017
  * Time: 19:12
  */
+//
+// $localhost = 'localhost';
+// $username = "root";
+// $password = "";
+// $dbname = "cfg_project";
 
-$localhost = 'localhost';
-$username = "root";
-$password = "";
-$dbname = "cfg_project";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$user = $url["user"];
+$pwd = $url["pass"];
+$dbname = substr($url["path"], 1);
 
 $connect = new mysqli($localhost, $username, $password, $dbname);
 // check connection
@@ -19,6 +25,3 @@ if($connect->connect_error) {
     // echo "Successfully connected";
 }
 ?>
-
-
-
